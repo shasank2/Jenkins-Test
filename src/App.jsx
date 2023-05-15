bc23d42c34dc4443a0cf25db193bfc063824704d
shasank2 <shasankshakya2@gmail.com>
2023-05-15 11:31:51 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://www.jenkins.io" target="_blank">
+          <img src={"https://www.jenkins.io/images/logos/jenkins/Jenkins-stop-the-war.svg"} className="logo" alt="Jenkins logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Jenkins + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and see if pipeline gets deployed or not.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
